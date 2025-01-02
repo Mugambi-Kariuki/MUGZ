80420c60d7ccb862b9e7911ebee7fa58cec1783a
Mugambi-Kariuki <yballer110@gmail.com>
2025-01-02 05:42:26 +0300

    updated commit

--- fetch_list.php
+++ fetch_list.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once "dbconnect.php";
+
+$sql = "select * from  api_ex";
+
+$stmt = $conn->query($sql);
+
--- page_two.php
+++ page_two.php
@@ -3,11 +3,13 @@
 session_start();
 
 if(isset($_SESSION['connect'])){
-    print'session is alive'<h3>. $SESSION["connect"].</h3> ;
+    print'session is alive'<h3>. $SESSION['connect'].</h3> ;
     print '<a href="page_three.php">destroy session</a>'
 }else{
     print 'session was destroyed'<br> <a <a href="page_one.php">Start new session</a>;
 }
 
 ?>
+
+
 
